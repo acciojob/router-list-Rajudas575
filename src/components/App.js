eac60b43de792e802404c42dd87af6b9c92277ed
Rajudas575 <rajudas575@gmail.com>
2025-01-06 13:41:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ItemList from "./components/ItemList";
-import ItemDetail from "./components/ItemDetail";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
 
 const App = () => {
   return (
